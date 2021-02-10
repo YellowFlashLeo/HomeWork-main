b416eb4e981263dcc097e1dafdc7ebf10a3ae711
Leonid Lobanov <leonid.lobanov@ericsson.com>
2021-02-10 16:01:17 +0200

    Added Swagger Support

--- HomeWorkTrial/Startup.cs
+++ HomeWorkTrial/Startup.cs
@@ -5,6 +5,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Number.Core;
 using Number.Db;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace HomeWorkTrial
 {
@@ -25,6 +28,22 @@ namespace HomeWorkTrial
             services.AddTransient<IinputServices, InputServices>();
             services.AddControllers().AddJsonOptions(options =>
                options.JsonSerializerOptions.PropertyNamingPolicy = null);
+            // Swagger configuration
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",
+                    new Microsoft.OpenApi.Models.OpenApiInfo
+                    {
+                        Title = "Swagger support for Fibonacci",
+                        Description = "Demo Api Swagger Support",
+                        Version = "v1"
+                    });
+                // Since it is name.xml
+                var fileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                // executing file + file name
+                var filePath = Path.Combine(AppContext.BaseDirectory, fileName);
+                options.IncludeXmlComments(filePath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +63,12 @@ namespace HomeWorkTrial
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+            // Adds Swagger Middleware to ASP.Core
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger support for Fibonacci");
             });
             
         }
